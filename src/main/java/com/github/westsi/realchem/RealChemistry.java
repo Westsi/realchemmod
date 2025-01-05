8beb4f7243e911c723674ba4265d72e4e7f6d6be
Westsi <76999267+Westsi@users.noreply.github.com>
2025-01-05 18:04:41 +0000

    Initial commit

---
+++ src/main/java/com/github/westsi/realchem/RealChemistry.java
@@ -0,0 +1,15 @@
+package com.github.westsi.realchem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RealChemistry implements ModInitializer {
+	public static final String MOD_ID = "realchem";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/github/westsi/realchem/RealChemistryClient.java
@@ -0,0 +1,10 @@
+package com.github.westsi.realchem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RealChemistryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
