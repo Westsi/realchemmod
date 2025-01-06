be884397e70c62f7ba59be7745c10e3fc1bce0ac
Westsi <76999267+Westsi@users.noreply.github.com>
2025-01-06 20:34:06 +0000

    Tinting?!

--- src/main/java/com/github/westsi/realchem/RealChemistryClient.java
+++ src/main/java/com/github/westsi/realchem/RealChemistryClient.java
@@ -9,6 +9,6 @@ public class RealChemistryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 //        RealChemistry.LOGGER.warn("INITIALIZING CLIENT");
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0xFF0000, ModItems.BASE_DUST);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> (tintIndex == 1 ? 15582019 : -1), ModItems.BASE_DUST);
     }
 }
